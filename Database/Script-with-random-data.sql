4a5b9075fd8494eb7afcb175694087825e667a50
Loocker90 <92094747+Loocker90@users.noreply.github.com>
2022-11-04 18:20:35 +0100

    Update Script-with-random-data.sql

--- Database/Script-with-random-data.sql
+++ Database/Script-with-random-data.sql
@@ -8,7 +8,7 @@
 --  FROM [dbo].[Customer]
 
   INSERT INTO [dbo].[Customer] 
-  VALUES (1, 'jc_sync_user', 'Œródmieœcie Pó³nocne, Warsaw, 00-863 Warsaw',52.230952, 21.006715)
+  VALUES (1, 'jc_sync_user', 'Å’rÃ³dmieÅ“cie PÃ³Â³nocne, Warsaw, 00-863 Warsaw',52.230952, 21.006715)
 
   --SELECT TOP (1000) [Id]
   --    ,[LogoLink]
@@ -27,7 +27,7 @@
 			'https://www.pngfind.com/pngs/m/587-5870909_free-reebok-png-reebok-transparent-png.png',
 			1,
 			30,
-			'This is the promotion of today some boots !',
-			'We got a 40 % on some boots',
+			'This is the promotion of today some boots!',
+			'We got a 40 % on some boots - remember to use Millennium debit or credit card!',
 			null
 			)
